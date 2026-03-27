9efe1fe14f7a8bdb44872e4269907d7a5f8f3f9f
WindowCleanerPT <devotedfroglover@gmail.com>
2026-03-27 16:39:38 -0700

    Update main.mod.js

--- polyqol/1.0.0/main.mod.js
+++ polyqol/1.0.0/main.mod.js
@@ -1,12 +1,24 @@
 import { PolyMod } from "https://cdn.polymodloader.com/cb/PolyTrackMods/PolyModLoader/0.6.0/PolyTypes.js";
 
 class SoundboardMod extends PolyMod {
+    play(url) {
+        new Audio(url).play();
+    }
+
     init = (pml) => {
         this.pml = pml;
         pml.registerBindCategory("Soundboard");
-        pml.registerKeybind("Beep", "sb_beep", "keydown", "KeyZ", null, () => {
-            const audio = new Audio("https://actions.google.com/sounds/v1/cartoon/beep_short.ogg");
-            audio.play();
+        pml.registerKeybind("Vine Boom", "sb_vineboom", "keydown", "Digit1", null, () => {
+            this.play("https://raw.githubusercontent.com/WindowCleanerPT/polyqol/main/sounds/vineboom.mp3");
+        });
+        pml.registerKeybind("Car Honk", "sb_honk", "keydown", "Digit2", null, () => {
+            this.play("https://raw.githubusercontent.com/WindowCleanerPT/polyqol/main/sounds/honk.mp3");
+        });
+        pml.registerKeybind("Beep", "sb_beep", "keydown", "Digit3", null, () => {
+            this.play("https://raw.githubusercontent.com/WindowCleanerPT/polyqol/main/sounds/beep.mp3");
+        });
+        pml.registerKeybind("Motorcycle Rev", "sb_rev", "keydown", "Digit4", null, () => {
+            this.play("https://raw.githubusercontent.com/WindowCleanerPT/polyqol/main/sounds/rev.mp3");
         });
     }
 }
