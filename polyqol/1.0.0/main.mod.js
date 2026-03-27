ecf95fef94c380e2bc034e25c195390defa21d49
WindowCleanerPT <devotedfroglover@gmail.com>
2026-03-27 16:28:31 -0700

    Update main.mod.js

--- polyqol/1.0.0/main.mod.js
+++ polyqol/1.0.0/main.mod.js
@@ -4,6 +4,10 @@ class SoundboardMod extends PolyMod {
     init = (pml) => {
         this.pml = pml;
         pml.registerBindCategory("Soundboard");
+        pml.registerKeybind("Beep", "sb_beep", "keydown", "KeyZ", null, () => {
+            const audio = new Audio("https://actions.google.com/sounds/v1/cartoon/beep_short.ogg");
+            audio.play();
+        });
     }
 }
 
