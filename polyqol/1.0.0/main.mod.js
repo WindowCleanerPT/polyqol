5aeff4efd1dc61844d26c5e77ad8091ede630c74
WindowCleanerPT <devotedfroglover@gmail.com>
2026-04-23 22:15:23 -0700

    Update main.mod.js

--- polyqol/1.0.0/main.mod.js
+++ polyqol/1.0.0/main.mod.js
@@ -1,7 +1,8 @@
 import { PolyMod } from "https://cdn.polymodloader.com/cb/PolyTrackMods/PolyModLoader/0.6.0/PolyTypes.js";
 
 pml.editorExtras.registerCategory(5, 5);
-pml.editorExtras.registerModel(pyramid.glb);
+pml.editorExtras.registerModel(Pyramid.glb);
+pml.editorExtras.registerBlock(1, 5, 21313, Pyramid, Pyramid, [[0,0,0],[1,1,1]],);
 
 }
 
